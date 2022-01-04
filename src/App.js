4a55d6986e20f311a9ea20c4334a551dd1f4f530
Marco <marco_trabajo@yahoo.com>
2022-01-05 00:11:55 +0100

    React JS - Context API - Marco testing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import Component1 from "./Components/Component1";
+import {ContextComponent3}  from "./Context_Exercise/ContextApi2022";
 
 function App() {
+  const [name, setName] = useState("Marco");
+  const [car, setCar] = useState("BMW 8");
+
   return (
     <div className="App">
-      <h1>Phase 2: React JS - Context API, React Query </h1>
+      {/* <ContextComponent3.Provider value={name, setName, car, setCar}> */}
+      <ContextComponent3.Provider value={[name, car]}>
+          <h1>Phase 2: React JS - Context API, React Query </h1>
+          <Component1 />
+      </ContextComponent3.Provider>
     </div>
   );
 }
