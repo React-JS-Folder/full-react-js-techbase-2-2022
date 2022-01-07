a9880fed38497477f1a709bf98b5d597a3f0eb18
Marco <marco_trabajo@yahoo.com>
2022-01-07 23:31:57 +0100

    React JS - React Query - fetching data - the way 95% of the cases use fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useQuery } from 'react-query'
+
+const fetchUsers = async () => {
+  const res = await fetch('https://reqres.in/api/users?');
+  return res.json();
+}
+
 
 function App() {
+  const {data, isLoading, isFetching, error} = useQuery("fetch Users", fetchUsers);
+  // destructuring the json object ---useQuery has a data property and the object has a data property  = data.data.map()
+
+  // if(isLoading) return 'Loading...';
+
   return (
     <div className="App">
-      <h1>Phase 2: React JS - Context API, React Query </h1>
+      <h1>React Query 3</h1>
+      <p>React Query is awesome!</p>
+
+      {data && data.data.map((user) => {
+          return <p key={user.id} >{user.id} : {user.email} </p>
+      })}
+
     </div>
   );
 }
