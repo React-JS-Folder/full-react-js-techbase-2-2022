80414b29bd47c084e28d4e8c49325a189721fb3e
Marco <marco_trabajo@yahoo.com>
2022-01-10 10:48:04 +0100

    React JS - React Query - useQuery onClick - enabled:false, refetch

--- src/components/RQSuperHeroespage.js
+++ src/components/RQSuperHeroespage.js
@@ -1,3 +1,37 @@
+import axios from 'axios'
+import { useQuery } from 'react-query'
+
+const fetchSuperHeroes = async () => {
+   // return axios.get('https://reqres.in/api/users?page=2');
+   const res = await axios.get('https://reqres.in/api/users?page=2');
+   console.log(res);
+   return res;
+}
+
+
+
 export default function RQSuperHeroesPage() {
-   return <div>RQ Super Heroes Page</div>
+   const { isLoading, data, error, isError, isFetching, refetch } = useQuery(
+      'super heroes',
+      fetchSuperHeroes,
+      {
+         enabled: false,
+      }
+   )
+
+
+
+   return (
+      <>
+      <div>RQ Super Heroes Page</div>
+      <p>Fetch data from url - on click</p>
+      <button onClick={refetch}>Fetch data</button>
+      {data && data.data.data.map((hero) => {
+         return (
+            <p key={hero.id}>{hero.first_name} email: {hero.email}</p>
+         )
+      })}
+      </>
+
+   )
 }
