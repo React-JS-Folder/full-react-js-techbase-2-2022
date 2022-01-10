5691210177ee65814c8778227131e10dd15be079
Marco <marco_trabajo@yahoo.com>
2022-01-10 14:09:46 +0100

    React JS - React Query 3 - success - callback - useQuery

--- src/components/RQSuperHeroespage.js
+++ src/components/RQSuperHeroespage.js
@@ -2,13 +2,18 @@ import axios from 'axios'
 import { useQuery } from 'react-query'
 
 const fetchSuperHeroes = async () => {
-   // return axios.get('https://reqres.in/api/users?page=2');
-   const res = await axios.get('https://reqres.in/api/users?page=2');
+   const res = await axios.get('https://reqres.in/api/users?page=2');     // this is success
    console.log(res);
    return res;
 }
 
+const executeFunctionOnSuccess = (error) => {
+   alert("useQuery was used to fetch data, and it has successfully done its job");
+}
 
+const onError = (error) => {
+   console.log("This is the error flag as a call back using useQuery " + error.message)
+}
 
 export default function RQSuperHeroesPage() {
    const { isLoading, data, error, isError, isFetching, refetch } = useQuery(
@@ -16,21 +21,22 @@ export default function RQSuperHeroesPage() {
       fetchSuperHeroes,
       {
          enabled: false,
+         onSuccess: executeFunctionOnSuccess,
+         onError,
       }
    )
 
-
-
    return (
       <>
-      <div>RQ Super Heroes Page</div>
-      <p>Fetch data from url - on click</p>
-      <button onClick={refetch}>Fetch data</button>
-      {data && data.data.data.map((hero) => {
-         return (
-            <p key={hero.id}>{hero.first_name} email: {hero.email}</p>
-         )
-      })}
+         <div>RQ Super Heroes Page</div>
+         <p>Fetch data from url - on click</p>
+         <button onClick={refetch}>Fetch data</button>
+         {data && data.data.data.map((hero) => {
+            return (
+               <p style={!error && { backgroundColor: '#000', color: '#ff0000'} }key={hero.id}>{hero.first_name} email: {hero.email}</p>
+            )
+         })}
+         
       </>
 
    )
